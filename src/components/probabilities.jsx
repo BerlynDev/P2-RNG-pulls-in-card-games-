36f26454110f9d128a65f07f661c6ed9456a4283
Bernardo López <bernardolopezf.dev@gmail.com>
2025-04-29 17:27:03 -0500

    feat: update components To_Do: P revision

--- src/components/probabilities.jsx
+++ src/components/probabilities.jsx
@@ -1,6 +1,16 @@
+
+
 export function Probabilities () {
+
     const cards = [["a", "b", "c", "d", "e", "f", "g"], ["h", "i", "j", "k", "l", "m"], ["n", "o", "p", "q", "r", "s", "t"], ["u", "v", "w", "x", "y", "z"], "ñ"] 
+
     const p = [0.40, 0.30, 0.20, 0.10, 0.01]
+
+    //const SubRandomLetter = cards[Math.floor(Math.random() * cards.length)][Math.floor(Math.random() * cards.length)]
+
+    /* 
+    Tb puedo hacerlo como objeto, pool1: ["a", "b", "c", "d", "e", "f", "g"] y al resultado hacer q de un random de cualquiera de esas letras?
+    */
 
     // function proba (x) {
     //     if (x < 0.40) {return cards[0]}
@@ -9,12 +19,13 @@ export function Probabilities () {
     //     else if (x < 0.99) {return cards[3]}
     //     else {return cards[4]}
     // }
+
     function proba (x) {
         if (x < p[0]) {return cards[0]}
-        else if (x < p[0] + p[1]) {return cards[1]}
-        else if (x < p[0] + p[1] + p[2]) {return cards[2]}
-        else if (x < p[0] + p[1] + p[2] + p[3]) {return cards[3]}
-        else {return cards[4]}
+        else if (x < p[0] + p[1]) {return cards[1][Math.floor(Math.random() * cards[1].length)]}
+        else if (x < p[0] + p[1] + p[2]) {return cards[2][Math.floor(Math.random() * cards[2].length)]}
+        else if (x < p[0] + p[1] + p[2] + p[3]) {return cards[3][Math.floor(Math.random() * cards[3].length)]}
+        else {return cards[4][Math.floor(Math.random() * cards[4].length)]}
     }
 
     for (j=0; j<100; j++) {
@@ -24,7 +35,7 @@ export function Probabilities () {
     }
 
     return (
-        proba(x)
+        proba(cards)
     )
 }  
 
@@ -35,3 +46,8 @@ https://medium.com/arcvision-blog/choosing-objects-probabilistically-in-javascri
 
 https://dustinpfister.github.io/2021/12/24/js-javascript-example-item-probability/
 */
+
+/*
+Por el momento no esta definido las probabilidades de aparicion, ya que todo depende de x, que es un numero aleatorio, y no de en si el rango de probabilidades
+*/   
+
